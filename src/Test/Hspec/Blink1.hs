c7fbbef95dda274afbb24e180de812af235d06ee
Bob Long <robertjflong@gmail.com>
2016-04-13 14:34:58 +0100

    refactor with Free

--- src/Test/Hspec/Blink1.hs
+++ src/Test/Hspec/Blink1.hs
@@ -1,33 +1,46 @@
+{-# LANGUAGE DeriveFunctor     #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Test.Hspec.Blink1 where
 
 import           Control.Concurrent
+import           Control.Monad.Free
 import qualified Data.Text              as T
 import           Shelly
 import           Test.Hspec             hiding (pending)
 import           Test.Hspec.Core.Runner
-import           Test.Hspec.Core.Spec   hiding (pending)
+import           Test.Hspec.Core.Spec   hiding (Pending, pending)
 
-blink :: [T.Text] -> Sh T.Text
-blink = verbosely . run "blink1-tool"
+data BlinkActionF r = Passed r | Pending r | Failed r deriving (Functor)
+type BlinkAction a = Free BlinkActionF a
 
-failed :: Sh T.Text
-failed = blink ["--rgb=255,0,0"]
+passed' :: BlinkAction ()
+passed' = liftF (Passed ())
 
-pending :: Sh T.Text
-pending = blink ["--rgb=255,165,0"]
+pending' :: BlinkAction ()
+pending' = liftF (Pending ())
 
-passed :: Sh T.Text
-passed = blink ["--rgb=0,255,0"]
+failed' :: BlinkAction ()
+failed' = liftF (Failed ())
+
+blink' :: BlinkActionF (IO a) -> IO a
+blink' (Passed r) = blink1Tool ["--rgb=0,255,0"] >> r
+blink' (Failed r) = blink1Tool ["--rgb=255,0,0"] >> r
+blink' (Pending r) = blink1Tool ["--rgb=255,165,0"] >> r
+
+blink1Tool :: [T.Text] -> IO T.Text
+blink1Tool = shelly . run "blink1-tool"
+
+blinkIO :: BlinkAction a -> IO a
+blinkIO = iterM blink'
 
 blink1 :: Spec -> IO Summary
 blink1 s = do
-  shelly pending
+  blinkIO pending'
   summary <- hspecResult s
   case summaryFailures summary of
-    0 -> shelly passed
-    _ -> shelly failed
+    0 -> blinkIO passed'
+    _ -> blinkIO failed'
   return summary
 
 example :: Spec
